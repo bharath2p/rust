c51efa7352fc0f089cf5a60daea49d941e3021b7
bharath2p <bharath2p@someemail.com>
2024-11-22 22:05:52 +0100

    Basic TCP server/client communication. Each client is processed by a thread.

--- simple_server_client/src/tcpclient.rs
+++ simple_server_client/src/tcpclient.rs
@@ -1,7 +1,20 @@
+use std::io::{BufRead, BufReader};
 use std::net::TcpStream;
 
 fn handle_connection(stream: TcpStream) {
-    log::info!("I am on!")
+    log::info!("Handling the connection...");
+    let mut reader = BufReader::new(&stream);
+    loop {
+        let mut text = String::new();
+        match reader.read_line(&mut text) {
+            Ok(result) => {
+                log::info!("Read: len = {}, data = {}", result, text);
+            }
+            Err(e) => {
+                log::info!("Read failed. Err({})", e);
+            }
+        }
+    }
 }
 pub fn client(ip_addr: String, port: u16) {
     log::info!("Establishing connection to {}:{}", ip_addr, port);
--- simple_server_client/src/tcpserver.rs
+++ simple_server_client/src/tcpserver.rs
@@ -1,9 +1,20 @@
+use std::io::{BufWriter, Write};
+use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
+use std::time;
 
-fn handle_new_connection(tcp_stream: TcpStream) {
+fn handle_new_connection(stream: TcpStream, addr: SocketAddr) {
     log::info!("Handling tcp stream ");
+    let mut writer = BufWriter::new(&stream);
+    let second = time::Duration::from_secs(1);
+    loop {
+        let send = format!("Hello {}\n", addr);
+        let _ = writer.write(send.as_bytes());
+        let _ = writer.flush();
+        thread::sleep(second);
+    }
 }
 pub fn server(ip_addr: String, port: u16) {
     log::info!("Starting server on {}:{}", ip_addr, port);
@@ -16,9 +27,9 @@ pub fn server(ip_addr: String, port: u16) {
         match tcp_listener.accept() {
             Ok((stream, addr)) => {
                 log::info!("Accepting connection from {}", addr);
-                thread::spawn(|| {
+                thread::spawn(move || {
                     log::info!("creating new thread to handle tcp stream...");
-                    handle_new_connection(stream);
+                    handle_new_connection(stream, addr);
                 });
             }
             Err(e) => log::info!("Failed to accept: Err ({})", e),
