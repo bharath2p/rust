4ccbee40c6bc925639d31bd60aef3ebfeb56cd56
bharath2p <bharath2p@someemail.com>
2025-01-08 22:22:20 +0100

    Updated data_stream main.rs with CLI

--- data_stream/src/main.rs
+++ data_stream/src/main.rs
@@ -1,3 +1,42 @@
+mod protos;
+use clap::{Parser, Subcommand};
+use protobuf::Message;
+use protos::stats::Statistics;
+use std::path::Path;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(about = "Data stream example...")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    ConfigFile { path: String },
+}
+
+fn read_file(path: String) {
+    log::info!("Reading file {}", path);
+    if !Path::new(&path).exists() {
+        log::info!("{} File does not exists...", path);
+        return;
+    } else {
+        log::info!("{} File exists...", path);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    let msg = Statistics::new();
+    println!("msg = {}, size = {}", msg, Message::compute_size(&msg));
+    let buff = vec![0; Message::compute_size(&msg).try_into().unwrap()];
+    */
+    femme::start();
+    log::info!("Starting application...");
+    let cli: Cli = Cli::parse();
+    match cli.command {
+        Commands::ConfigFile { path } => read_file(path),
+    }
 }
